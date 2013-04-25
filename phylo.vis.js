cce9bc80964315cbc9da1a906b56293691503a94
Tim Vaughan <tgvaughan@gmail.com>
2013-04-26 10:26:39 +1200

    Started work on layout object.

---
+++ phylo.vis.js
@@ -0,0 +1,15 @@
+// Tree layout object
+var Layout = {
+    tree: undefined,
+    nodePositions: {},
+
+    init: function(tree) {
+	this.tree = tree;
+	this.nodePositions = {};
+    },
+
+    // Produce a standard 
+    standard: function() {
+    }
+}
+

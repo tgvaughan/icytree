5663bd8c18698a16a217c9ca09ea21c280f71a6e
Tim Vaughan <tgvaughan@gmail.com>
2013-04-25 13:35:29 +1200

    Lexer working.

--- phylo.js
+++ phylo.js
@@ -40,17 +40,17 @@ var TreeFromNewick = Object.create(Tree, {
     newick: {value: "", configurable: true, writable: true, enumerable: true},
     idx: {value: 0, writable: true, configurable: false},
     tokens: {value: [
-	["OPENP", "\\(", false],
-	["CLOSEP", "\\)", false],
-	["COLON", ":", false],
-	["COMMA", ",", false],
-	["SEMI", ";", false],
-	["OPENA", "\\[&", false],
-	["CLOSEA", "\\]", false],
+	["OPENP", /\(/, false],
+	["CLOSEP", /\)/, false],
+	["COLON", /:/, false],
+	["COMMA", /,/, false],
+	["SEMI", /;/, false],
+	["OPENA", /\[&/, false],
+	["CLOSEA", /\]/, false],
 	["STRING", "\"\\w+\"", true],
-	["EQ", "=", false],
-	["NUM", "-?\\d+(\\.\\d+)?([Ee]-?\\d+)?", true],
-	["LABEL", "\\w+", true]
+	["EQ", /=/, false],
+	["NUM", /-?\d+(\.\d+)?([Ee]-?\d+)?/, true],
+	["LABEL", /\w+/, true]
     ], writeable: false, configurable: false},
 
     // Initialiser
@@ -68,11 +68,18 @@ var TreeFromNewick = Object.create(Tree, {
     
 	while (this.idx<this.newick.length) {
 
+	    // Skip over whitespace:
+	    var wsMatch = this.newick.slice(this.idx).match(/\s/);
+	    if (wsMatch != null && wsMatch.index == 0) {
+		this.idx += wsMatch[0].length;
+		continue;
+	    }
+
 	    var matchFound = false;
 	    for (var k = 0; k<this.tokens.length; k++) {
 		var match = this.newick.slice(this.idx).match(this.tokens[k][1]);
 		if (match != null && match.index == 0) {
-		    matchFound = true;
+
 		    if (this.tokens[k][2]) {
 			tokenList.push([this.tokens[k][0],match[0]]);
 			console.log(this.idx + " " + this.tokens[k][0] + ": " + match[0]);
@@ -81,6 +88,7 @@ var TreeFromNewick = Object.create(Tree, {
 			console.log(this.idx + " " + this.tokens[k][0]);
 		    }
 
+		    matchFound = true;
 		    this.idx += match[0].length
 		    break;
 		}
@@ -100,6 +108,9 @@ var TreeFromNewick = Object.create(Tree, {
 
 function main() {
     
-    var tree = Object.create(TreeFromNewick).init("(A[&deme=\"1\",reaction=\"sample\"]:1,B:1)C:0;");
+    var newickString = document.getElementById("newickInput").innerHTML;
+    newickString = newickString.replace(/&amp;/g,"&");
+    var tree = Object.create(TreeFromNewick).init(newickString);
+    console.log("Done!");
 
 }

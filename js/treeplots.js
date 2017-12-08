cb19dfef700c1144295c2d24cbe05599a8e0ba8a
Tim Vaughan <tgvaughan@gmail.com>
2017-12-08 18:20:44 +0100

    Playing with skyline implementation.

--- js/treeplots.js
+++ js/treeplots.js
@@ -23,8 +23,50 @@ var TreePlots = (function () {
         Plotly.newPlot(divName, [trace], layout);
     }
 
+    function getSkylineLogL(data, effectiveN, intervalEndTimes) {
+        var logL = 0.0;
+
+        var dt, k, dk, coalRate;
+
+        var treeIntervalIdx = 0;
+        var treeIntervalStartTime = 0.0;
+
+        var S = 0;
+
+        for (var i=0; i<intervalEndTimes.length; i++) {
+
+            N = effectiveN[i];
+
+            while (data.ages[treeIntervalIdx]<=intervalEndTimes[i]) {
+                dt = data.ages[treeIntervalIdx] - treeIntervalStartTime;
+                k = data.lineages[i];
+
+                if (dt>0)
+                    S += 1;
+
+                treeIntervalStartTime = data.ages[treeIntervalIdx];
+                treeIntervalIdx += 1;
+
+                coalRate = k*(k-1)/2/N;
+                logL += -dt*coalRate;
+
+                dk = data.lineages[i+1] - data.lineages[i];
+                if (dk<0) {
+                    logL += Math.log(coalRate); 
+                }
+            }
+        }
+
+        var K = effectiveN.length;
+
+        return logL - K  + K*(K+1)/(S-K-1);
+    }
+
     function drawSkyline(divName, epsilon) {
         var data = trees[currentTreeIdx].getLineagesThroughTime();
+
+        // Coalescent trees have 1 lineage remaining above root node
+        data.lineages.push(1);
 
         if (epsilon === undefined)
             epsilon = 0.0;
@@ -34,13 +76,15 @@ var TreePlots = (function () {
         var effectiveN = [];
         var intervalEndTimes = [];
 
-        intervalStartTime = data.ages[0];
+        var intervalStartTime = 0.0;
+        var prevTime = 0.0;
         var accumulator = 0.0;
         var nCoals = 0;
 
-        for (var i=0; i<data.lineages.length-1; i++) {
-            dt = data.ages[i+1]-data.ages[i]
-            k = data.lineages[i]
+        for (var i=0; i<data.lineages.length; i++) {
+            dt = data.ages[i]-prevTime;
+            prevTime = data.ages[i];
+            k = data.lineages[i];
 
             accumulator += dt*k*(k-1)/2;
 
@@ -51,17 +95,19 @@ var TreePlots = (function () {
             if (isCoal)
                 nCoals += -dk;
 
-            if (isCoal && data.ages[i+1]-intervalStartTime>=epsT) {
+            if (isCoal && data.ages[i]-intervalStartTime>epsT) {
                 // Coalescence event
 
-                intervalEndTimes.push(data.ages[i+1]);
+                intervalEndTimes.push(data.ages[i]);
                 effectiveN.push(accumulator/nCoals);
 
-                intervalStartTime = data.ages[i+1];
+                intervalStartTime = data.ages[i];
                 accumulator = 0.0;
                 nCoals = 0;
             }
         }
+
+        console.log("logL = " + getSkylineLogL(data, effectiveN, intervalEndTimes));
 
         var trace = {
             x: [0].concat(intervalEndTimes),
